36a4d30391905ffd845be2dcab77234953626a03
FedericoFores <fedefores1234567@gmail.com>
2022-03-05 01:18:24 -0300

    Primer boceto del home

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const path = require("path");
-const id = 3000;
+const id = 3030;
 const app = express();
 const publicPath = path.join(__dirname,"../","public");
 
 app.use(express.static(publicPath));
 
 app.listen(id, () => {
-    console.log("Servidor en funcionamiento en el puerto"+ id);
+    console.log("Servidor en funcionamiento en el puerto "+ id);
 });
 
 app.get('/', function(req,res){
-    res.sendFile(path.join(dirname,"./views/home.html"));
+    res.sendFile(path.join(__dirname,"./views/home.html"));
 });
 
 app.get('/producto', function(req,res){
-    res.sendFile(path.join(dirname,"./views/...")); /* poner su archivo donde estan los puntitos */
+    res.sendFile(path.join(__dirname,"./views/...")); /* poner su archivo donde estan los puntitos */
 });
 
 app.get('/login', function(req,res){
-    res.sendFile(path.join(dirname,"./views/..."));
+    res.sendFile(path.join(__dirname,"./views/..."));
 });
 
 app.get('/register', function(req,res){
-    res.sendFile(path.join(dirname,"./views/..."));
+    res.sendFile(path.join(__dirname,"./views/..."));
 });
 
 app.get('/carrito', function(req,res){
