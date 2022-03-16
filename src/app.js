9cfbe14941e7f418d6fd1628d7be55ad2d47e2de
Fran452 <Franciscolemacr@gmail.com>
2022-03-15 23:40:40 -0300

    eliminando la carpeta node

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const path = require("path");
-const id = 3000;
+const PORT = process.env.PORT || 3000;;
 const app = express();
+
 const publicPath = path.join(__dirname,"../","public");
 
 app.use(express.static(publicPath));
 
-app.listen(id, () => {
-    console.log("Servidor en funcionamiento en el puerto"+ id);
+app.listen(PORT, () => {
+    console.log("Servidor en funcionamiento en el puerto"+ PORT);
 });
 
 app.get('/', function(req,res){
-    res.sendFile(path.join(dirname,"./views/home.html"));
+    res.sendFile(path.join(__dirname,"./views/home.html"));
 });
 
 app.get('/producto', function(req,res){
-    res.sendFile(path.join(dirname,"./views/...")); /* poner su archivo donde estan los puntitos */
+    res.sendFile(path.join(__dirname,"./views/...")); /* poner su archivo donde estan los puntitos */
 });
 
 app.get('/login', function(req,res){
-    res.sendFile(path.join(dirname,"./views/..."));
+    res.sendFile(path.join(__dirname,"./views/..."));
 });
 
 app.get('/register', function(req,res){
-    res.sendFile(path.join(dirname,"./views/..."));
+    res.sendFile(path.join(__dirname,"./views/..."));
 });
 
 app.get('/carrito', function(req,res){
