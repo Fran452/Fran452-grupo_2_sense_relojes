6c657a97e61eef8dfd7ae73e8dad509e19e5df10
Fran452 <Franciscolemacr@gmail.com>
2022-03-18 11:04:10 -0300

    creacion estructura de carpeta routers y controllers con un archivo de prueba

---
+++ controllers/productosControllers.js
@@ -0,0 +1,6 @@
+const controlador = {
+    index:"funcion al momento de entrar a al index de productos",
+    show: "funcion para ingresar al detalle del producto",
+    create: "para ingresar datos para la creacion de un produto"
+}
+module.exports = controlador;
