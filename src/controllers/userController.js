09d497cf0439edeadb2722b275ec8b9ec3a7b536
Fran452 <Franciscolemacr@gmail.com>
2022-04-13 16:53:56 -0300

    verificacion del register terminada

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const {validationResult} = require("express-validator");
+
 const controlador = {
     login:(req,res) => {
         res.render("login")
@@ -8,7 +10,12 @@ const controlador = {
         res.render("register")
     },
     newUser : (req,res) => {
-        
+        let validaciones = validationResult(req);
+
+        if(validaciones.errors.length > 0){
+            return res.render("register",{error:validaciones.mapped()});
+        }
+        return res.send("usuario validado")
     }
 }
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const {body} = require("express-validator")
+
 const router = express.Router();
 const userController = require("../controllers/userController");
-
+const validaciones = [
+    body("nombre").isLength({ min: 5 }).withMessage('El nombre debe tener al menos 5 caracteres'),
+    body("email").isEmail().withMessage('El campo debe ser un email'),
+    body("telefono").isLength({ min: 10 }).withMessage('El telefono debe contar con a menos 10 caracteres'),
+    body("fechaDeNacimiento"),
+    body("contraseña").isLength({ min: 8 }).withMessage('la contraseña debe tener al menos 8 caracteres'),
+    body("contraseñaConfirm").custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Password confirmation does not match password');
+        }
+        return true;
+      }).withMessage('Las contraseñas no coinciden'),
+]
 
 
 
 router.get('/register',userController.register);
-router.post('/register',userController.newUser);
-
-router.get('/',userController.login);
+router.post('/',validaciones,userController.newUser);
 
 router.get('/login',userController.login);
 
