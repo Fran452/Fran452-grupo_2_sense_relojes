761f3dd4b60a8d3fe7d443eec1dae459d1e47550
YamilaBog <yambogarin@gmail.com>
2022-05-10 20:00:45 -0300

    Guardar pagina de usuario y error found

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,11 @@ const path = require("path");
 const controlador = {
     login:(req,res) => {res.render("login")},
     register:(req,res) => {res.render("register")},
+    paginausuario:(req,res) => {res.render("paginausuario")},
+    id:(req,res) => {
+        let productoSeleccionado = fuctionGeneric.archivoJSON(dataBase).find(producto => producto.id == req.params.id)
+        res.render("paginausuario",{producto:productoSeleccionado, productRecomiend : fuctionGeneric.archivoJSON(dataBase)})
+    }
 }
 
 module.exports = controlador;
