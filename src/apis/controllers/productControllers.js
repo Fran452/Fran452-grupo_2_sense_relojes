702a84e9419097382475756062eeb0d61f297e82
Fran452 <Franciscolemacr@gmail.com>
2022-06-26 19:27:32 -0300

    subiendo base de datos

--- src/apis/controllers/productControllers.js
+++ src/apis/controllers/productControllers.js
@@ -5,14 +5,18 @@ const { QueryTypes } = require('sequelize');
 const controlador = {
     index : async (req,res) => {
         let count = await dataBaseSQL.productos.findAll();
-        let countByCategory = await dataBaseSQL.sequelize.query('SELECT COUNT(*) FROM `Producto` GROUP By "tipo";');
+        let countByCategory = await dataBaseSQL.sequelize.query('SELECT tipo,COUNT(*) FROM `Producto` GROUP By tipo;');
         let products= await dataBaseSQL.productos.findAll();
-        count = count.length
-        res.json({count, products,countByCategory})
+        count = count.length;
+        countByCategory = countByCategory[0];
+
+        res.json({count, products,countByCategory});
     },
 
     detalle : async (req,res) => {
-        let producto = dataBaseSQL.producto.findByPk(req.params);
+        let producto = dataBaseSQL.producto.findByPk(req.params,{
+            
+        });
          
     }
 
