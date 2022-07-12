3ed8693f405dc23d9a740b65bf5ad81d6885cab3
Agustin Enriquez <99294180+AgusEnriquez@users.noreply.github.com>
2022-07-12 20:38:05 -0300

    cambio2

--- myapphuman/src/Components/TablaUsers/TablaUsers.js
+++ myapphuman/src/Components/TablaUsers/TablaUsers.js
@@ -1,28 +1,54 @@
 import React, {useEffect, useState} from "react";
 
-const fetchApi = (url) => {
-        fetch(url)
-        .then(response => {
-                console.log(response.json());
-                return response.json()}  )
-            .then(responseJSON => console.log(responseJSON))
-            .catch(Error => console.log(Error))
-        };
-        
-        function TablaUsers() {
-                const [apiUser, setApiUser] = useState([]);
-                useEffect(() => {
-                        fetchApi("http://localhost:3001/apis/users")
-                    },[]);
-                    return (
-                            <ul>
-                                {apiUser.length <= 0
-                                    ? "cargando.."
-                                    : apiUser.map((apiUser, index) => {
-                                                return <li> {apiUser.usuariosFinal.nombre} </li>;
-                                          })}
-                                </ul>
-                            );
-                        }
-                        
-                        export default TablaUsers;
+
+
+function TablaUsers(){
+    const [apiUser, setApiUser] = useState([])
+
+    useEffect(() => {
+        const getUsers = () => {
+            fetch("http://localhost:3001/apis/users")
+            .then(res => res.json()
+            .then(res => console.log(res)))
+        }
+        getUsers()
+    }, [])
+    return(
+        <ul>
+            {apiUser.length <= 0
+                 ? "cargando.."
+                 : apiUser.map((apiUser, index) => {
+                         return <li> {apiUser.usuariosFinal.nombre} </li>;
+                   })}
+         </ul>
+
+    )
+}
+
+
+export default TablaUsers;
+
+// const fetchApi = (url) => {
+//     fetch(url)
+//     .then(response => {
+//         console.log(response.json());
+//         return response.json()}  )
+//     .then(responseJSON => console.log(responseJSON))
+//     .catch(Error => console.log(Error))
+// };
+
+// function TablaUsers() {
+//     const [apiUser, setApiUser] = useState([]);
+//     useEffect(() => {
+//         fetchApi("http://localhost:3001/apis/users")
+//     },[]);
+//     return (
+//         <ul>
+//             {apiUser.length <= 0
+//                 ? "cargando.."
+//                 : apiUser.map((apiUser, index) => {
+//                         return <li> {apiUser.usuariosFinal.nombre} </li>;
+//                   })}
+//         </ul>
+//     );
+// }
