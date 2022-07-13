9f892cf9b96eb3bd4c8648508f0fec737b12418e
Fran452 <Franciscolemacr@gmail.com>
2022-07-12 22:22:09 -0300

    subiendo funcional TablaUsers.js

--- myapphuman/src/Components/TablaUsers/TablaUsers.js
+++ myapphuman/src/Components/TablaUsers/TablaUsers.js
@@ -1,13 +1,40 @@
 import React, {useEffect, useState} from "react";
 
 
+const fetchApi = (url) => {
+    return fetch(url)
+    .then(response => response.json())
+    .then(responseJSON =>responseJSON)
+    .catch(Error => console.log(Error))
+    
+};
 
+function TablaUsers() {
+    const [apiUser, setApiUser] = useState([]);
+  
+    useEffect(() => {
+        fetchApi("http://localhost:3001/apis/users")
+        .then(respuesta => setApiUser(respuesta));
+    },[]);
+
+    return (
+        <ul>
+            {apiUser.users?.length <= 0
+                ? "cargando.."
+                : apiUser && Array.isArray(apiUser.users) && apiUser.users.map((apiUser, index) => {
+                        return <li key = {apiUser.id + apiUser.nombre}> {apiUser.nombre} </li>;
+                  })}
+        </ul>
+    );
+}
+
+/*
 function TablaUsers(){
     const [apiUser, setApiUser] = useState([])
 
     useEffect(() => {
         const getUsers = () => {
-            fetch("http://localhost:3001/apis/users")
+            fetch("http: localhost:3001/apis/users")
             .then(res => res.json()
             .then(res => console.log(res)))
         }
@@ -24,31 +51,8 @@ function TablaUsers(){
 
     )
 }
-
+*/
 
 export default TablaUsers;
 
-// const fetchApi = (url) => {
-//     fetch(url)
-//     .then(response => {
-//         console.log(response.json());
-//         return response.json()}  )
-//     .then(responseJSON => console.log(responseJSON))
-//     .catch(Error => console.log(Error))
-// };
-
-// function TablaUsers() {
-//     const [apiUser, setApiUser] = useState([]);
-//     useEffect(() => {
-//         fetchApi("http://localhost:3001/apis/users")
-//     },[]);
-//     return (
-//         <ul>
-//             {apiUser.length <= 0
-//                 ? "cargando.."
-//                 : apiUser.map((apiUser, index) => {
-//                         return <li> {apiUser.usuariosFinal.nombre} </li>;
-//                   })}
-//         </ul>
-//     );
-// }
+  
